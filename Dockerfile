d682681f5333f2a3cfdc19bbf0afc1018243de73
Alberto Verna <alberto.verna@hotmail.com>
2025-07-16 17:33:18 +0200

    Update single-website analysis script and Docker container

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,33 @@
-FROM sitespeedio/browsertime:24.5.0
+ARG VERSION="2.0-beta"
+ARG PYTHON_VERSION="3.13"
+
+FROM salb98/extract-allowed-domains:${VERSION} AS extract-allowed
+
+FROM salb98/priv-accept-ps:${VERSION} AS crawler
+
+FROM salb98/priv-accept-post-process:${VERSION} AS post-process
+
+FROM python:${PYTHON_VERSION}-slim AS runner
+
+ARG PYTHON_VERSION
 
 # Install xargs and jq
-RUN apt update && apt install -y findutils jq xvfb
+RUN apt-get update && apt-get install -y findutils jq libglib2.0 libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libatspi2.0-0 libxcomposite1 libxdamage1 libgbm1 libxkbcommon0 libpango1.0-0 libcairo2 libxrender1 libcap2 libgcrypt20 liblzma5 libzstd1 liblz4-1 libblkid1 libgpg-error0 libmd0 xvfb
 
-# Add custom version of Chromium
-ADD ./crawler/chromium /opt/chromium-topics
+RUN mkdir -p /opt/priv-accept-ps
 
-# Create workdir
-RUN mkdir /opt/priv-accept-topics
-ADD crawler/ /opt/priv-accept-topics/crawler
-ADD analyze-topics-api/ /opt/priv-accept-topics/analyze-topics-api
-ADD analyze-topics-single.sh /opt/priv-accept-topics/analyze-topics.sh
+# Copy crawler, post-process and allowed domain extraction files
+COPY --from=extract-allowed /opt/extract-allowed-domains /opt/priv-accept-ps/extract-allowed-domains
+COPY --from=crawler /opt/priv-accept /root/* /opt/priv-accept-ps/crawler/
+COPY --from=crawler /opt/google-chrome /opt/google-chrome
+COPY --from=post-process /opt/priv-accept-post-process /opt/priv-accept-post-process/modules /opt/priv-accept-ps/post-process/
+COPY ./analyze-topics-single.sh /opt/priv-accept-ps/analyze-topics.sh
 
-# Install Python libraries
-RUN pip install -r /opt/priv-accept-topics/analyze-topics-api/requirements.txt
-RUN pip install -r /opt/priv-accept-topics/crawler/requirements.txt
+RUN pip install -r /opt/priv-accept-ps/extract-allowed-domains/requirements.txt -r /opt/priv-accept-ps/crawler/requirements.txt -r /opt/priv-accept-ps/post-process/requirements.txt
 
-WORKDIR /opt/priv-accept-topics
-ENTRYPOINT [ "bash", "/opt/priv-accept-topics/analyze-topics.sh" ]
+# Copy devtools patch
+COPY --from=crawler /usr/local/lib/python${PYTHON_VERSION}/site-packages/selenium/webdriver/common/devtools /usr/local/lib/python${PYTHON_VERSION}/site-packages/selenium/webdriver/common/devtools
+
+WORKDIR /opt/priv-accept-ps
+
+ENTRYPOINT [ "bash", "analyze-topics.sh" ]
--- analyze-topics-single.sh
+++ analyze-topics-single.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+# Exit script on any command error
+set -e
+
 # Environment variables
 WORKING_DIR="${WORKING_DIR:=$PWD}"
 INPUT_DIR="${INPUT_DIR:="$WORKING_DIR/input"}"
 OUTPUT_DIR="${OUTPUT_DIR:="$WORKING_DIR/output"}"
-CHROMIUM_DIR="${CHROMIUM_DIR:="$WORKING_DIR/crawler/chromium"}"
+CHROMIUM_DIR="${CHROMIUM_DIR:="/opt/google-chrome"}"
+
+# Check for parameters
+deny=0
+for arg in "$@"; do
+    if [ "$arg" == "--deny" ]; then
+        deny=1
+        break
+    fi
+done
 
 # Extract allowed domains if unavailable
-if [ ! -f "$INPUT_DIR/allowed-domains.txt" ]; then
-    echo "No list of allowed domains given, using empty list." >&2
+if [ ! -f "$INPUT_DIR/allowed-domains.csv" ]; then
+    echo "No list of allowed domains given, downloading allowed domains" >&2
     mkdir -p "$INPUT_DIR"
-    touch "$INPUT_DIR/allowed-domains.txt"
+    python3 "$WORKING_DIR/extract-allowed-domains/extract_allowed_domains.py" \
+        --api_map "$WORKING_DIR/extract-allowed-domains/ps_api_map.json" \
+        --output "$INPUT_DIR/allowed_domains.csv"
 fi
 
 if [ ! -d "$OUTPUT_DIR" ]; then
@@ -18,6 +32,7 @@ if [ ! -d "$OUTPUT_DIR" ]; then
 fi
 
 # RUN PRIV-ACCEPT
+echo "Running crawler"
 python3 "$WORKING_DIR/crawler/priv-accept.py" \
     --url "$1" \
     --outfile "$OUTPUT_DIR/priv-accept-output.json" \
@@ -25,44 +40,39 @@ python3 "$WORKING_DIR/crawler/priv-accept.py" \
     --chrome_driver "$CHROMIUM_DIR/chromedriver" \
     --chrome_binary "$CHROMIUM_DIR/chrome" \
     --accept_words "$WORKING_DIR/crawler/accept_words.txt" \
+    --deny_words "$WORKING_DIR/crawler/deny_words.txt" \
+    --option_words "$WORKING_DIR/crawler/option_words.txt" \
     --screenshot_dir "$OUTPUT_DIR/screenshots" \
     --lang "en, en-us, en-gb, it, fr, es, de, ru" \
     --docker --clear_cache --full_net_log --xvfb \
-    --detect_topics \
+    $( if [ "$deny" = 1 ]; then echo '--deny'; fi ) \
     $PRIV_ACCEPT_ARGS
 
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+visits_json='["first","second"]'
+fields_json='["contacted_domains","api_calls","partitioned_cookies"]'
 
-# EXTRACT AND ATTEST DOMAINS
-echo "domain,attestation_json" > "$OUTPUT_DIR/attested-domains.csv"
 echo "Extracting contacted domains"
-python3 "$WORKING_DIR/analyze-topics-api/extract-domains.py" "$OUTPUT_DIR/priv-accept-output.json" --visit first | sort | uniq > "$OUTPUT_DIR/contacted-domains-first.txt"
-python3 "$WORKING_DIR/analyze-topics-api/extract-domains.py" "$OUTPUT_DIR/priv-accept-output.json" --visit second | sort | uniq > "$OUTPUT_DIR/contacted-domains-second.txt"
-cat "$OUTPUT_DIR/contacted-domains-first.txt" "$OUTPUT_DIR/contacted-domains-second.txt" | sort | uniq > "$OUTPUT_DIR/contacted-domains.txt"
+echo "domain,attestation_json" > "$OUTPUT_DIR/attested-domains.csv"
+jq  -L "$WORKING_DIR/post-process/modules" \
+    -f "$WORKING_DIR/post-process/extract_contacted_2ld.jq" \
+    --argjson visits $visits_json \
+    --arg separate 1 \
+    --arg full_net_log 1 \
+    "$OUTPUT_DIR/priv-accept-output.json" \
+    > "$OUTPUT_DIR/contacted-domains.json"
+
+set +e
 echo "Checking for attested domains"
-cat "$OUTPUT_DIR/contacted-domains.txt" | xargs -I {} timeout -s KILL ${CONNECTION_TIMEOUT:=30} python3 "$WORKING_DIR/analyze-topics-api/attest-domain.py" {} >> "$OUTPUT_DIR/attested-domains.csv"
+jq -r 'flatten | unique[]' "$OUTPUT_DIR/contacted-domains.json" |
+xargs -I {} sh -c "timeout -s KILL ${CONNECTION_TIMEOUT:=30} python3 "$WORKING_DIR/post-process/attest-domain.py" {} || true" >> "$OUTPUT_DIR/attested-domains.csv"
+set -e
 
-# EXTRACT TOPICS API DATA FROM OUTPUTS
-python3 "$WORKING_DIR/analyze-topics-api/analyze-topics-api.py" "$OUTPUT_DIR/priv-accept-output.json" \
-    --attested_domains_file "$OUTPUT_DIR/attested-domains.csv" \
-    --allowed_domains_file "$INPUT_DIR/allowed-domains.txt" \
-    --consent_managers_file "$WORKING_DIR/analyze-topics-api/consent-managers.txt" \
-    --outfile "$OUTPUT_DIR/analyze-topics-output.json"
-
-if [ $? -ne 0 ]; then
-    exit 2
-fi
-
-# ADD CONTACTED DOMAINS INTO FINAL JSON
-jq -c \
-    --rawfile contacted_domains_first "$OUTPUT_DIR/contacted-domains-first.txt" \
-    --rawfile contacted_domains_second "$OUTPUT_DIR/contacted-domains-second.txt" \
-    '. + { first: (.first + { contacted_domains: ($contacted_domains_first | split("\n") | map(select(length > 0))) }), second: (.second + { contacted_domains: ($contacted_domains_second | split("\n") | map(select(length > 0))) }) }' \
-    "$OUTPUT_DIR/analyze-topics-output.json" \
+echo "Extracting meaningful data from crawler output"
+jq  -L "$WORKING_DIR/post-process/modules" \
+    -f "$WORKING_DIR/post-process/post_process_output.jq" \
+    --argjson visits $visits_json \
+    --argjson fields $fields_json \
+    --arg position -1 \
+    --arg full_net_log 1 \
+    --arg csv_format 0 \
     > "$OUTPUT_DIR/final-output.json"
-
-if [ $? -ne 0 ]; then
-    exit 3
-fi
