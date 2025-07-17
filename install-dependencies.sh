bf03e23187efdcb3931892310e017383613e45b4
Alberto Verna <alberto.verna@hotmail.com>
2025-07-17 17:24:13 +0200

    Remove un-needed dependencies for complete script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,12 +1,6 @@
 # Install apt packages
 sudo apt update
-sudo apt install wget python3 python3-pip python3-requests xvfb parallel coreutils zip unzip snapd
-
-# Download Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt -f install
-rm google-chrome-stable_current_amd64.deb
+sudo apt install parallel coreutils zip unzip snapd
 
 # Download and setup Docker
 sudo snap install docker
